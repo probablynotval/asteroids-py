edb4875dbbd3927f55965d827f610f69f9d9b241
probablynotval <162844653+probablynotval@users.noreply.github.com>
2024-09-10 17:23:00 +0000

    feat: player rotation

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN_SIZE)
     clock = pygame.time.Clock()
-    dt = 0
+    dt: float = 0
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -19,9 +19,10 @@ def main():
                 return
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
-        dt = clock.tick(60) / 1000
+        dt: float = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
--- src/player.py
+++ src/player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 from circleshape import CircleShape
 
 
@@ -18,3 +18,15 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def rotate(self, dt: float):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt: float):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+
+        if keys[pygame.K_d]:
+            self.rotate(dt)
